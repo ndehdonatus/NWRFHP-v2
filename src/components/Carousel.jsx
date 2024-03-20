7c27237fab5f309896609037e651e531ecd7eae3
ndehdonatus <ndehdonatus@gmail.com>
2024-03-20 15:35:37 +0100

    changes on carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,44 +7,53 @@ export default function DefaultCarousel() {
     return (
 
         <Carousel className="w-full h-80 object-cover  ">
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/096A0522.jpg"
             />
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/096A0583.jpg"
             />
+
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/096A0599.png"
             />
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/rock.jpg"
             />
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/head-office.jpg"
             />
 
-            <Image className='h-full'
+            {/* <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/nnfca.jpg"
-            />
+            /> */}
+
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/vanca.jpg"
             />
-            <Image className='h-full'
+
+            {/* <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/nkca.jpg"
-            />
+            /> */}
 
-            <Image className='h-full'
+            {/* <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/csmu1.jpg"
-            />
+            /> */}
 
 
             <Image className='h-full'
@@ -52,45 +61,50 @@ export default function DefaultCarousel() {
                 src="/images/fondoh1.jpg"
             />
 
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/yembe1.jpg"
             />
 
-            <Image className='h-full'
+            {/* <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/comca.jpg"
-            />
+            /> */}
 
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/odrana1.jpg"
             />
 
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/mbakong1.jpg"
             />
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/management1.jpg"
             />
-            <Image className='h-full'
+
+            {/* <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/fonca.jpg"
-            />
+            /> */}
+
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/ndehca.jpg"
             />
-            <Image className='h-full'
+            {/* <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/intenship.jpg"
-            />
-            <Image className='h-full'
+            /> */}
+            {/* <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/fon3.jpg"
-            />
+            /> */}
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/medalists.jpg"
